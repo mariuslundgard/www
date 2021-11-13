b8a1d6ffb32a2c440b1ad95a4d11d34d4bf2176e
Marius Lundgård <studio@mariuslundgard.com>
2021-11-13 19:12:41 +0100

    refactor(website): update favicons

--- website/document/CommonMetaTags.tsx
+++ website/document/CommonMetaTags.tsx
@@ -12,43 +12,58 @@ export function CommonMetaTags(props: {staticBasePath: string}) {
         title="Copyright Marius Lundgård"
         href="https://mariuslundgard.com/license"
       />
-      <link href={`${staticBasePath}/apple-touch-icon.png`} rel="apple-touch-icon" />
+
+      <link rel="apple-touch-icon" sizes="48x48" href={`${staticBasePath}/ml-favicon-48.png`} />
+      <link rel="apple-touch-icon" sizes="72x72" href={`${staticBasePath}/ml-favicon-72.png`} />
+      <link rel="apple-touch-icon" sizes="96x96" href={`${staticBasePath}/ml-favicon-96.png`} />
+      <link rel="apple-touch-icon" sizes="144x144" href={`${staticBasePath}/ml-favicon-144.png`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${staticBasePath}/ml-favicon-180.png`} />
+      <link rel="apple-touch-icon" sizes="192x192" href={`${staticBasePath}/ml-favicon-192.png`} />
+      <link rel="apple-touch-icon" sizes="256x256" href={`${staticBasePath}/ml-favicon-256.png`} />
+      <link rel="apple-touch-icon" sizes="384x384" href={`${staticBasePath}/ml-favicon-384.png`} />
+      <link rel="apple-touch-icon" sizes="512x512" href={`${staticBasePath}/ml-favicon-512.png`} />
+      <link rel="shortcut icon" href={`${staticBasePath}/favicon-64.png`} />
       <link
-        href={`${staticBasePath}/apple-touch-icon-precomposed.png`}
-        rel="apple-touch-icon-precomposed"
-      />
-      <link href={`${staticBasePath}/safari-pinned-tab.svg`} rel="mask-icon" color={black.hex} />
-      <link
-        href={`${staticBasePath}/android-chrome-192x192.png`}
-        sizes="192x192"
         rel="icon"
         type="image/png"
-      />
-      <link
-        href={`${staticBasePath}/favicon-96x96.png`}
-        sizes="96x96"
-        rel="icon"
-        type="image/png"
-      />
-      <link
-        href={`${staticBasePath}/favicon-32x32.png`}
-        sizes="32x32"
-        rel="icon"
-        type="image/png"
-      />
-      <link
-        href={`${staticBasePath}/favicon-16x16.png`}
         sizes="16x16"
+        href={`${staticBasePath}/ml-favicon-16.png`}
+      ></link>
+      <link
         rel="icon"
         type="image/png"
+        sizes="32x32"
+        href={`${staticBasePath}/ml-favicon-32.png`}
       />
-      <link rel="manifest" href={`${staticBasePath}/manifest.json`} />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="48x48"
+        href={`${staticBasePath}/ml-favicon-48.png`}
+      />
+      <link rel="apple-touch-icon" sizes="57x57" href={`${staticBasePath}/ml-favicon-57.png`} />
+      <link rel="apple-touch-icon" sizes="60x60" href={`${staticBasePath}/ml-favicon-60.png`} />
+      <link rel="apple-touch-icon" sizes="72x72" href={`${staticBasePath}/ml-favicon-72.png`} />
+      <link rel="apple-touch-icon" sizes="76x76" href={`${staticBasePath}/ml-favicon-76.png`} />
+      <link rel="apple-touch-icon" sizes="114x114" href={`${staticBasePath}/ml-favicon-114.png`} />
+      <link rel="apple-touch-icon" sizes="120x120" href={`${staticBasePath}/ml-favicon-120.png`} />
+      <link rel="apple-touch-icon" sizes="144x144" href={`${staticBasePath}/ml-favicon-144.png`} />
+      <link rel="apple-touch-icon" sizes="152x152" href={`${staticBasePath}/ml-favicon-152.png`} />
+      <link rel="apple-touch-icon" sizes="167x167" href={`${staticBasePath}/ml-favicon-167.png`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={`${staticBasePath}/ml-favicon-180.png`} />
+      <link
+        rel="apple-touch-icon"
+        sizes="1024x1024"
+        href={`${staticBasePath}/ml-favicon-1024.png`}
+      />
+
+      <link rel="manifest" href={`${staticBasePath}/ml-manifest.json`} />
       <meta name="apple-mobile-web-app-title" content="Marius Lundgård" />
       <meta name="apple-mobile-web-app-capable" />
       <meta name="application-name" className="app-title" content="Marius Lundgård" />
       <meta
         name="theme-color"
-        content={theme.color.dark.accent?.default?.default?.enabled.bg || black.hex}
+        content={theme.color.dark.base?.default?.default?.enabled.bg || black.hex}
       />
     </>
   )
